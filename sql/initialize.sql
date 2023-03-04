631b587bc929a9bcef95849e860cb3faa0a1eb89
yui-Kitamura <yui@yui-kitamura.eng.pro>
2023-03-04 14:34:02 +0900

    database initialize DDL

---
+++ sql/initialize.sql
@@ -0,0 +1,29 @@
+create schema pro_eng_yui_samples;
+
+CREATE TABLE `pro_eng_yui_samples`.`m_worker` (
+    `person_id` INT NOT NULL,
+    `name` VARCHAR(45) NULL,
+    `birth` DATE NULL,
+    `note` VARCHAR(100) NULL,
+    `LgcDelFlg` TINYINT NULL,
+    PRIMARY KEY (`person_id`)
+)
+COMMENT = 'master table for work persons';
+
+CREATE TABLE `pro_eng_yui_samples`.`t_payment` (
+    `person_id` int NOT NULL,
+    `payment_id` int NOT NULL,
+    `payed_date` date DEFAULT NULL,
+    `amount` decimal(10,0) DEFAULT NULL,
+    `note` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`person_id`,`payment_id`),
+    UNIQUE KEY `payment_id_UNIQUE` (`payment_id`)
+)
+COMMENT='transactional data table for payment';
+
+CREATE TABLE `pro_eng_yui_samples`.`m_salary` (
+    `pay_rank_id` INT NOT NULL,
+    `amount` DECIMAL NULL,
+    PRIMARY KEY (`pay_rank_id`)
+)
+COMMENT = 'based payment amount ';
