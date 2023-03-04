a4a703b335d7f6f23bd2887728a7677d9b62ceb5
yui-Kitamura <yui@yui-kitamura.eng.pro>
2023-03-04 16:37:19 +0900

    override from Object

--- src/main/java/pro/eng/yui/samples/callProcWithMybatis/dto/colType/ErrMsg.java
+++ src/main/java/pro/eng/yui/samples/callProcWithMybatis/dto/colType/ErrMsg.java
@@ -11,4 +11,29 @@ public class ErrMsg {
         this.msg = msg;
     }
 
+    @Override
+    public String toString(){
+        return get();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 27, key = 13;
+        hash = hash * key + msg.hashCode();
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof ErrMsg objMsg){
+            boolean isSame = false;
+            //noinspection RedundantIfStatement
+            if(this.msg.equals(objMsg.msg)){
+                isSame = true;
+            }
+            return isSame;
+        }else{
+            return false;
+        }
+    }
 }
